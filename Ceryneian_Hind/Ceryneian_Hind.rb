d7df9c39f32d75098b268b3ead0cb8d79bb97865
Liam Prior <liamprior44@gmail.com>
2018-03-09 16:30:51 -0800

    slight error fix

--- Ceryneian_Hind/Ceryneian_Hind.rb
+++ Ceryneian_Hind/Ceryneian_Hind.rb
@@ -6,7 +6,7 @@
 #    By: lprior <lprior@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/03/08 13:20:44 by lprior            #+#    #+#              #
-#    Updated: 2018/03/09 16:15:01 by lprior           ###   ########.fr        #
+#    Updated: 2018/03/09 16:30:29 by lprior           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,31 +20,28 @@ UID = ENV["SUID"]
 SECRET = ENV["SECRET"]
 client = OAuth2::Client.new(UID, SECRET, site: "https://api.intra.42.fr")
 token = client.client_credentials.get_token
+response = token.get("/v2/users")
+until response.status == 200 do
+    puts "Waiting on connection".bold.red    
+    response = token.get("/v2/users")
+    sleep(5.seconds)
+end
 if ARGV[0]
     if File.extname(ARGV[0]) == ".txt"
         File.open(ARGV[0]).each_line do |line|
             username = line.chomp
             begin
-                def internet
-                    begin
-                      true if open("https://api.intra.42.fr")
-                    rescue
-                      false
-                    end
-                  end
                 users = token.get("/v2/users/#{username}").parsed
-                if users["location"] && internet == true
+                if users["location"]
                     puts "#{username}s".blue + " first name is" + " #{users["first_name"]}".bold.underline.blue + " he/she is" + " available".green + ", logged into" + \
                             " #{users["location"]}".light_blue + ", is" + " LVL #{users["cursus_users"][0]["level"]}".magenta + \
                                 ", and has" + " #{users["correction_point"]}".yellow + " correction points!"
-                elsif internet == true
+                else
                     puts "OH!".red + " It Seems" + " #{username}".blue + " is not logged in ¯\\_(ツ)_/¯"
                 end
             rescue
                 if ("#{username}" == "")
                     puts "No New Lines please".red
-                elsif (internet == false)
-                    puts "server is down! Try again!".bold.red
                 else
                     puts "#{username} ".blue + "does not exist".red
                 end
